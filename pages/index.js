5e90878096cc6884599a33348a1e0408e4e2f626
lvwenrui <lvwenrui12@live.cn>
2023-10-07 00:47:22 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,121 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Lock from '../artifacts/contracts/Lock.sol/Lock.json'
+import { ethers } from 'ethers';
+ 
+const connect=async ()=>{
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+  await provider.send("eth_requestAccounts", []);
+
+  const signer = provider.getSigner()
+}
+
+const call_contract= async ()=>{
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+  await provider.send("eth_requestAccounts", []);
+
+  const signer = provider.getSigner()
+
+
+  const daiAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+ 
+  const daiContract = new ethers.Contract(daiAddress, Lock.abi, signer);
+
+}
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+       <button onClick={connect}>链接钱包</button>
+       <button onClick={call_contract}>设置</button>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
